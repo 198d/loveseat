8e3ba3a93d46e72e11523c844a825c9afd552b3b
John MacKenzie <john@nineteeneightd.com>
2010-09-29 08:55:29 -0500

    Properties
    Add _id and _rev properties automatically at initialization.  Add
    accessor to the properties hash.

--- stoner/document/support.rb
+++ stoner/document/support.rb
@@ -1,25 +1,44 @@
 module Stoner
   module Document
     class Support
+      attr_accessor :properties
+
       def initialize(klass)
         @klass = klass
-        @properties = []
+        @properties = {}
         Stoner::Document.registry[@klass] = self
+        add_property(:_id, Types::String.new)
+        add_property(:_rev, Types::String.new)
       end
 
       def integer(name, default = nil)
         property = Types::Integer.new(default)
-        add_property(property)
+        add_property(name,property)
       end
 
       def string(name, default = nil)
         property = Types::String.new(default)
-        add_property(property)
+        add_property(name,property)
+      end
+
+      def float(name, default = nil)
+        property = Types::Float.new(default)
+        add_property(name,property)
+      end
+      
+      def date(name, default = nil)
+        property = Types::Date.new(default)
+        add_property(name,property)
+      end
+      
+      def time(name, default = nil)
+        property = Types::Time.new(default)
+        add_property(name,property)
       end
 
       private 
-        def add_property(property)
-          @properties << property
+        def add_property(name,property)
+          @properties[name.to_sym] = property
           @klass.class_eval do
             define_method(name.to_sym) do
               property.get
