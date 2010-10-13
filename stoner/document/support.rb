563896bb1522fc505adf4ebcd7470e5459f240ac
John MacKenzie <john@nineteeneightd.com>
2010-10-12 23:58:10 -0500

    Abstract Class Support
    Add information to support abstract classes which won't get saved
    to the database but can be used for embedded documents, mapping
    arbitrary views to objects and hopefully more.

--- stoner/document/support.rb
+++ stoner/document/support.rb
@@ -3,18 +3,21 @@ module Stoner
     class Support
       attr_accessor :properties, :dsl
 
-      def initialize(klass)
+      def initialize(klass, options = {})
         @klass = klass
         @properties = {}
         @dsl = DSL.new(self)
+        @abstract = !!options[:abstract]
         
         klass.class_eval("
           def __stoner_document
             @__stoner_document ||= {}
           end
         ")
-        add_property(:_id, Types::String.new)
-        add_property(:_rev, Types::String.new)
+        unless @abstract
+          add_property(:_id, Types::String.new)
+          add_property(:_rev, Types::String.new)
+        end
       end
 
       def add_property(name,property)
@@ -34,7 +37,7 @@ module Stoner
         doc = {}
         properties.each do |k,v|
           value = instance.send(k)
-          doc[k] = value unless v.empty?(value)
+          doc[k] = value unless v.class.empty?(value)
         end
         doc.to_json
       end
@@ -47,48 +50,9 @@ module Stoner
         object
       end
 
-      private
-
-        class DSL
-          def initialize(support)
-            @support = support 
-          end
-
-          def integer(name, default = nil)
-            property = Types::Integer.new(default)
-            @support.add_property(name,property)
-          end
-
-          def string(name, default = nil)
-            property = Types::String.new(default)
-            @support.add_property(name,property)
-          end
-
-          def float(name, default = nil)
-            property = Types::Float.new(default)
-            @support.add_property(name,property)
-          end
-          
-          def date(name, default = nil)
-            property = Types::Date.new(default)
-            @support.add_property(name,property)
-          end
-          
-          def time(name, default = nil)
-            property = Types::Time.new(default)
-            @support.add_property(name,property)
-          end
-
-          def hash(name, default = nil)
-            property = Types::Hash.new(default)
-            @support.add_property(name,property)
-          end
-          
-          def array(name, default = nil)
-            property = Types::Array.new(default)
-            @support.add_property(name,property)
-          end
-        end
+      def abstract?
+        @abstract
+      end
     end
   end
 end
