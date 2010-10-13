6765c81faf59bf954555cdea53f48dcff1f20e1f
John MacKenzie <john@nineteeneightd.com>
2010-10-12 23:56:57 -0500

    Generalize Document API
    In preparation for design document support, generalize the API a
    document needs. This can be built on to suppor the special design
    document.

--- stoner/document.rb
+++ stoner/document.rb
@@ -1,60 +1,11 @@
 module Stoner
   module Document
     @@registry = {}
-
-    def self.setup(klass, &block)
-      support = Support.new(klass)
-      registry[klass.name] = support
-      yield(support.dsl)
-      support
+    
+    def self.registry
+      @@registry
     end
-
-    def self.put(db, object)
-      support = registry[object.class.name]
-      raise "Not Registered" if support.nil?
-      
-      if object._id.nil?
-        object._id = next_id(db.server, object.class)
-      end
-
-      resource = Rest::Document.new(db, object._id)
-      response, body = resource.put(support.to_doc(object))
-
-      response.value
-
-      object._rev = body["rev"]
-      object
-    end
-
-    def self.get(db, id)
-      resource = Rest::Document.new(db, id)
-      response, body = resource.get
-      response.value
-
-      klass, uuid = id.split(':')
-      support = registry[klass]
-      support.from_hash(body)
-    end
-
-    def self.all(db, klass)
-      support = registry[klass.name]
-      resource = db._all_docs
-      response, body = resource.get(:query => {:startkey => "#{klass.name}:".to_json,
-                                               :endkey => "#{klass.name}:\u0fff".to_json,
-                                               :include_docs => true})
-      response.value
-
-      body['rows'].map do |row|
-        support.from_hash(row['doc'])
-      end
-    end
-
-    def self.delete(db, object)
-      resource = Rest::Document.new(db, object._id)
-      response, body = resource.delete(:query => {:rev => object._rev})
-      response.value
-    end
-
+    
     def self.next_id(server, klass)
       response, body = server._uuids.get
       response.value
@@ -62,10 +13,69 @@ module Stoner
       uuid = body['uuids'].first
       "#{klass.name}:#{uuid}"
     end
-    
-    def self.registry
-      @@registry
+
+    def self.resolve(id)
+      klass, uuid = id.split(':')
+      klass
     end
+    
+    module API
+      def setup(klass, options = {}, &block)
+        support = self::Support.new(klass, options)
+        registry[klass.name] = support
+        yield(support.dsl) if block_given?
+        support
+      end
+
+      def put(db, object)
+        support = registry[object.class.name]
+        raise "Not Registered" if support.nil?
+        raise "Abstract Document" if support.abstract?
+        
+        if object._id.nil?
+          object._id = next_id(db.server, object.class)
+        end
+
+        resource = Rest::Document.new(db, object._id)
+        response, body = resource.put(support.to_doc(object))
+
+        response.value
+
+        object._rev = body["rev"]
+        object
+      end
+
+      def get(db, id)
+        resource = Rest::Document.new(db, id)
+        response, body = resource.get
+        response.value
+
+        klass = resolve(id)
+        support = registry[klass]
+        support.from_hash(body)
+      end
+
+      def all(db, klass)
+        support = registry[klass.name]
+        resource = db._all_docs
+        response, body = resource.get(:query => {:startkey => "#{klass.name}:".to_json,
+                                                 :endkey => "#{klass.name}:\u0fff".to_json,
+                                                 :include_docs => true})
+        response.value
+
+        body['rows'].map do |row|
+          support.from_hash(row['doc'])
+        end
+      end
+
+      def delete(db, object)
+        resource = Rest::Document.new(db, object._id)
+        response, body = resource.delete(:query => {:rev => object._rev})
+        response.value
+      end
+
+    end
+    extend Api
   end
 end
 
